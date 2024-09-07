868535fb50b062dd65dff87c58da6b27a7166c7f
Amaterisa <isabella-cordeiro@hotmail.com>
2024-09-07 13:46:43 -0400

    Adjust go to top position and avoid recreating search adapter

--- app/src/main/java/com/amaterisa/movielistapp/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/amaterisa/movielistapp/presentation/search/SearchFragment.kt
@@ -54,21 +54,23 @@ class SearchFragment : BaseFragment() {
 
     private fun setupBinding() {
         binding.run {
-            moviesRv.adapter = adapter
-            val spaceInPixels = resources.getDimensionPixelSize(R.dimen.half_default_padding)
-            moviesRv.addItemDecoration(LinearItemDecoration(spaceInPixels, true))
+            if (moviesRv.adapter == null) {
+                moviesRv.adapter = adapter
+                val spaceInPixels = resources.getDimensionPixelSize(R.dimen.half_default_padding)
+                moviesRv.addItemDecoration(LinearItemDecoration(spaceInPixels, true))
 
-            moviesRv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                    super.onScrolled(recyclerView, dx, dy)
+                moviesRv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
 
-                    if (dy > 0) {
-                        btnTop.visibility = View.VISIBLE
-                    } else if (!recyclerView.canScrollVertically(-1)) {
-                        btnTop.visibility = View.GONE
+                        if (dy > 0) {
+                            btnTop.visibility = View.VISIBLE
+                        } else if (!recyclerView.canScrollVertically(-1)) {
+                            btnTop.visibility = View.GONE
+                        }
                     }
-                }
-            })
+                })
+            }
 
             btnTop.setOnClickListener {
                 moviesRv.smoothScrollToPosition(0)
